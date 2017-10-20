6e0c8f753129743d6c63913968bfe4b66daa733f
MohammedFouad <mohfoad@gmail.com>
2017-10-20 23:45:19 +0200

    modify countdown to be minutes and seconds inside the circle

--- app/src/main/java/com/mohamedfoad/myapplication/pomodoroActivity.java
+++ app/src/main/java/com/mohamedfoad/myapplication/pomodoroActivity.java
@@ -9,20 +9,24 @@ import android.widget.TextView;
 
 import com.github.lzyzsd.circleprogress.DonutProgress;
 
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 public class pomodoroActivity extends AppCompatActivity {
     TextView mTextField;
     EditText userTime;
     long test;
     DonutProgress progress;
-int time = 25000;
+    int time = 25000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pomodoro);
-
+        mTextField = (TextView) findViewById(R.id.textCountdown);
         progress = (DonutProgress) findViewById(R.id.donut_progress);
 
-       progress.setMax(25);
+        progress.setMax(25);
     }
 
     public void startCountdownPomodoro(View view) {
@@ -30,8 +34,13 @@ int time = 25000;
         new CountDownTimer(1500000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-               // mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
-                progress.setProgress(millisUntilFinished / 1000/60);
+
+                progress.setProgress(millisUntilFinished / 1000 / 60);
+
+                String text = String.format(Locale.getDefault(), "%02d:%02d",
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) % 60,
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60);
+                mTextField.setText(text);
 
             }
 
